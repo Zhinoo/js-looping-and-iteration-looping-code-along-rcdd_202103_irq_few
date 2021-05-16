c91c6fbcaa09e9874174ccbfb84805a653b898c3
Zhino Salam <zhinosalam188@gmail.com>
2021-05-16 21:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ writeCards([
   "Thank you, Brendan, for the wonderful birthday gift!",
   "Thank you, Ali, for the wonderful birthday gift!"
 ]);
+
+function printBadges(employees){
+  for (i=0; i < employees.length; i++){
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees;
+}
+
+
+function tailsNeverFails(){
+  let tails = Math.random() >= 0.5
+  let counter = 0;
+
+  while (tails) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`
+}
