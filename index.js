6e92cf6fb5f758685e0b59b60334fa00c92035b3
Zhino Salam <zhinosalam188@gmail.com>
2021-05-16 21:26:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+let writeCards= (["Ada", "Brendan", "Ali"], "birthday");
+writeCards([
+  "Thank you, Ada, for the wonderful birthday gift!",
+  "Thank you, Brendan, for the wonderful birthday gift!",
+  "Thank you, Ali, for the wonderful birthday gift!"
+]);
