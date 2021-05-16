c43aecabc4fa571bb4a12e2658c284f26a8af343
Zhino Salam <zhinosalam188@gmail.com>
2021-05-16 21:27:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // Code your solutions in this file
-let writeCards= (["Ada", "Brendan", "Ali"], "birthday");
-writeCards([
-  "Thank you, Ada, for the wonderful birthday gift!",
-  "Thank you, Brendan, for the wonderful birthday gift!",
-  "Thank you, Ali, for the wonderful birthday gift!"
-]);
 
 function printBadges(employees){
   for (i=0; i < employees.length; i++){
